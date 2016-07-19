7f8aa8575a7369663f62382392ff936fc10961d1
Marc Bruins <marciebruins@gmail.com>
2016-07-19 18:50:13 +0200

    initial commit

---
+++ XFCardBoard/VRPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace XFCardBoard
+{
+	public class VRPage : ContentPage
+	{
+		public VRPage()
+		{
+			
+		}
+	}
+}
+
+
---
+++ XFCardBoardApp/MainPage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace XFCardBoardApp
+{
+	public class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			var button = new Button
+			{
+				Text = "Click Me!",
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+			};
+
+			int clicked = 0;
+			button.Clicked += (s, e) => button.Text = "Clicked: " + clicked++;
+
+			Content = button;
+		}
+	}
+}
+
+
